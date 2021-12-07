84de055070356938d101c7ff2171a061d4532746
Max Pronko <max.pronko@gmail.com>
2021-12-07 08:00:44 +0000

    First commit

---
+++ Controller/Post/Index.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Basics\Controller\Post;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    private $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute(): ResultInterface
+    {
+        $page = $this->pageFactory->create();
+        $page->addHandle('some_random_handle');
+
+        $page->getLayout()->getBlock('magemastery.basics.handles')
+            ->setData('handles', $page->getLayout()->getUpdate()->getHandles());
+
+        return $page;
+    }
+}
